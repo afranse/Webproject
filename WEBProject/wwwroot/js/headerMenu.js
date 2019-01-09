193894500fa346ed7709f2cdacd436516d4b08f4
jel07 <jel07@users.noreply.github.com>
2019-01-09 15:59:21 +0100

    Add menu javascript and jquery

---
+++ WEBProject/wwwroot/js/headerMenu.js
@@ -0,0 +1,24 @@
+﻿// Used documents:
+// http://api.jquery.com/
+// https://www.w3schools.com/jquery/
+
+var menu = true;
+
+$(document).ready(function () {
+    $(window).click(function () {
+        if ($("#menu_js").is("#menu_js:visible") == true && menu == true) {
+            $("#menu_js").hide();
+        } else {
+            menu = true;
+        }
+    });
+
+    $("#menu_button_js").click(function () {
+        $("#menu_js").toggle();
+        menu = false;
+    });
+
+    $("#menu_content_js").click(function () {
+        menu = false;
+    });
+});
