21e6fe2964278b2777d211d80496d2f7c6f56b71
DaicaQuang <35957696+DaicaQuang@users.noreply.github.com>
2019-01-16 12:26:22 +0100

    Update layout of recipe page & Add hover to footer certificate buttons

--- WEBProject/Controllers/RecipesController.cs
+++ WEBProject/Controllers/RecipesController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEBProject.Data;
 
 namespace WEBProject.Controllers
 {
     public class RecipesController : Controller
     {
+        private readonly WebsiteContext _context;
+
+        public RecipesController(WebsiteContext context)
+        {
+            _context = context;
+            //new Seeder(_context);
+        }
+
         public IActionResult Index()
         {
             return View();
