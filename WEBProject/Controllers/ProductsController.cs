7cc9e9bff8ed851eca0a2093c285f4465566ea5e
Justin <17042623@student.hhs.nl>
2019-01-17 09:59:27 +0100

    ProductController refinements and start tests

--- WEBProject/Controllers/ProductsController.cs
+++ WEBProject/Controllers/ProductsController.cs
@@ -45,20 +45,30 @@ namespace WEBProject.Controllers
         }
 
         //convert the string array to Type_Category List
-        private List<Models.Type_Category> getTypes(string[] typestring)
+        public List<Models.Type_Category> getTypes(string[] typestring)
         {
             List<Models.Type_Category> selectedTypes = new List<Models.Type_Category>();
             foreach (string s in typestring)
             {
-                int i = 0;
-                int.TryParse(s.Substring(2), out i);
-                selectedTypes.Add(_context.Type_Categories.Where(x => x.TypeID.Equals(i)).FirstOrDefault());
+                int i = -1;
+                if (s.Count() > 2)
+                {
+                    int.TryParse(s.Substring(2), out i);
+                    if (i != -1)
+                    {
+                        var result = _context.Type_Categories.Where(x => x.TypeID.Equals(i)).FirstOrDefault();
+                        if (result != null)
+                        {
+                            selectedTypes.Add(result);
+                        }
+                    }
+                }
             }
             return selectedTypes;
         }
 
         //convert the string array to Normal_Category List
-        private List<Models.Normal_Category> GetCategories(string[] catstring)
+        public List<Models.Normal_Category> GetCategories(string[] catstring)
         {
             List<Models.Normal_Category> selectedCategories = new List<Models.Normal_Category>();
             foreach (string s in catstring)
@@ -78,16 +88,105 @@ namespace WEBProject.Controllers
             //The query to search products
             ViewBag.Products = GetProducts(BranchID, selectedTypes, selectedCategories);
             //all Branches
-            ViewBag.AllBranches = _context.Branch_Categories.ToList();
+            ViewBag.AllBranches = getBranches();
             //all Types from certain Branch
-            ViewBag.AllTypes = _context.Type_Categories.Where(s => s.BranchCategory.BranchID.Equals(BranchID)).ToList();
+            ViewBag.AllTypes = getAllTypes(BranchID);
             //all Categories from certain Types
-            ViewBag.AllCat = (from C in _context.Normal_Categories
-                              join TC in selectedTypes on C.TypeID equals TC.TypeID
-                              select C).ToList(); ;
+            ViewBag.AllCat = getAllCategorie(selectedTypes);
+        }
+
+        public List<Models.Branch_Category> getBranches()
+        {
+            if(_context.Branch_Categories != null)
+            {
+                return _context.Branch_Categories.ToList();
+            }
+            else
+            {
+                return new List<Models.Branch_Category>();
+            }
+        }
+
+        public List<Models.Normal_Category> getAllCategorie(List<Models.Type_Category> selectedTypes)
+        {
+            var AllCats = (from C in _context.Normal_Categories
+                           join TC in selectedTypes on C.TypeID equals TC.TypeID
+                           select C);
+            if(AllCats != null)
+            {
+                return AllCats.ToList();
+            }
+            else
+            {
+                return new List<Models.Normal_Category>();
+            }
+        }
+
+        public List<Models.Type_Category> getAllTypes(int BranchID)
+        {
+            var AllTypes = _context.Type_Categories.Where(s => s.BranchCategory.BranchID.Equals(BranchID));
+            if(AllTypes != null)
+            {
+                return AllTypes.ToList();
+            }
+            else
+            {
+                return new List<Models.Type_Category>();
+            }
         }
 
 
+
+
+        public IActionResult Category(string[] types, string[] Categories, int Branch)
+        {
+
+            return RedirectToAction("Index", new { typestring = types, catstring = Categories, BranchID = Branch - 1 });
+        }
+
+
+
+        public IActionResult SpecificProduct(int ID)
+        {
+            Models.Product P = _context.Products.Where(p => p.ArticleNumber == ID).FirstOrDefault();
+            return View(P);
+        }
+
+
+
+        public List<Models.Product> GetProducts(int BranchID, List<Models.Type_Category> selectedTypes, List<Models.Normal_Category> selectedCategories)
+        {
+            var producten = (from p in _context.Products
+                             join ncp in _context.NormalCategory_Products on p.ArticleNumber equals ncp.ArticleNumber
+                             join nc in _context.Normal_Categories on ncp.CategoryID equals nc.CategoryID
+                             join tc in _context.Type_Categories on nc.TypeID equals tc.TypeID
+                             join bc in _context.Branch_Categories on tc.BranchID equals bc.BranchID
+                             join stc in selectedTypes on tc.TypeID equals stc.TypeID
+                             join snc in selectedCategories on nc.CategoryID equals snc.CategoryID
+                             where (bc.BranchID == BranchID)
+                             select p);
+
+            //To catch any exceptions
+            if (producten != null)
+            {
+                return producten.ToList();
+            }
+            else
+            {
+                return new List<Models.Product>();
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------
         public IActionResult Filter(int id)
         {
             return RedirectToAction("Index", new { BranchID = id-1});
@@ -111,47 +210,9 @@ namespace WEBProject.Controllers
             ViewData["message"] = message;
             return View();
         }
+        //---------------------------------------------------------------------------------------------------------------------------------------------------------
 
-        public IActionResult Category(string[] types, string[]Categories, int Branch)
-        {
-            
-            return RedirectToAction("Index", new {typestring = types, catstring = Categories, BranchID = Branch-1});
-        }
-
-        public IActionResult SpecificProduct (int ID)
-        {
-            Models.Product P = _context.Products.Where(p => p.ArticleNumber == ID).FirstOrDefault();
-            return View(P);
-        }
-
-
-
-
-
-        public List<Models.Product> GetProducts(int BranchID, List<Models.Type_Category> selectedTypes, List<Models.Normal_Category> selectedCategories)
-        {
-            var producten = (from p in _context.Products
-                             join ncp in _context.NormalCategory_Products on p.ArticleNumber equals ncp.ArticleNumber
-                             join nc in _context.Normal_Categories on ncp.CategoryID equals nc.CategoryID
-                             join tc in _context.Type_Categories on nc.TypeID equals tc.TypeID
-                             join bc in _context.Branch_Categories on tc.BranchID equals bc.BranchID
-                             join stc in selectedTypes on tc.TypeID equals stc.TypeID
-                             join snc in selectedCategories on nc.CategoryID equals snc.CategoryID
-                             where (bc.BranchID == BranchID)
-                             select p);
-
-            //To catch any exceptions
-            if (producten != null)
-            {
-               return producten.ToList();
-            }
-            else
-            {
-                return new List<Models.Product>();
-            }
-        }
-
-
+        
 
 
     }
--- XUnitTesten/ProductsControllerTest.cs
+++ XUnitTesten/ProductsControllerTest.cs
@@ -7,19 +7,45 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 
+
 namespace XUnitTesten
 {
     public class ProductsControllerTest
     {
+
+        private InMemoryDB IMDB = new InMemoryDB();
+        private WEBProject.Data.WebsiteContext context;
+        private ProductsController c;
+
+        public ProductsControllerTest()
+        {
+            context = IMDB.GetInMemoryDB(true);
+            SeederInMemoryDB.Seeder_Chicken(context);
+            c = new ProductsController(context);
+        }
+
         [Fact]
         public void IndexInProductsController()
         {
-            ProductsController c = new ProductsController();
-            var result = c.Index();
+            string[] array1 = new string[0];
+            string[] array2 = new string[0]; 
+            var result = c.Index(0, array1, array2);
 
             //result moet een view zijn
             var viewResult = Assert.IsType<ViewResult>(result);
             Assert.Null(viewResult.ViewName);
         }
+
+        [Fact]
+        public void getSelectedTypes()
+        {
+            string[] array = new string[] {"A", "B 2", "C987"};
+            var result = c.getTypes(array);
+
+
+
+        }
+
+
     }
 }
