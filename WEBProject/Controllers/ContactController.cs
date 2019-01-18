cfb263ff08688b76bac913c81be1e9f9dfe7f443
Alexander <sfafranse@hotmail.com>
2019-01-18 11:15:27 +0100

    Add context constructor

--- WEBProject/Controllers/ContactController.cs
+++ WEBProject/Controllers/ContactController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WEBProject.Data;
+using WEBProject.Models;
 
 namespace WEBProject.Controllers
 {
     public class ContactController : Controller
     {
+        private readonly WebsiteContext _context;
+
+        public ContactController(WebsiteContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            
             return View();
         }
 
