f0403db17c6c40a11de309315a284018b03adc40
Justin <17042623@student.hhs.nl>
2019-01-18 12:15:07 +0100

    Added Products and Subscription Tests [*still needs PK fixes*]

---
+++ XUnitTesten/SubscriptionControllerTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Xunit;
+using WEBProject.Models;
+using WEBProject.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace XUnitTesten
+{
+    public class SubscriptionControllerTest
+    {
+        private InMemoryDB DB;
+        private WEBProject.Data.WebsiteContext context;
+        private SubscriptionController c;
+
+        public SubscriptionControllerTest()
+        {
+            DB = new InMemoryDB();
+            context = DB.GetInMemoryDB(true);
+            SeederInMemoryDB.Seed(context);
+            c = new SubscriptionController(context);
+        }
+
+        [Fact]
+        public void subscribeTest()
+        {
+            context = DB.GetInMemoryDB(true);
+            string mail = "Test@Testmail.Test";
+            c.subscribe(mail);
+            Assert.Equal(context.Subscribers.ToList()[0].Email, mail);
+        }
+        [Fact]
+        public void unsubscribeTest()
+        {
+            string mail = "Test@Testmail.Test";
+            c.Unsubscribe(mail);
+            Assert.Empty(context.Subscribers.ToList());
+        }
+    }
+}
