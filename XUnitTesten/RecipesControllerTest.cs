8baa18e6d447c2bcac1f413219bbac01c459d037
DaicaQuang <35957696+DaicaQuang@users.noreply.github.com>
2019-01-19 16:26:42 +0100

    Make new method GetRecipes in RecipesController & Add TestGetRecipes test method

--- XUnitTesten/RecipesControllerTest.cs
+++ XUnitTesten/RecipesControllerTest.cs
@@ -11,15 +11,36 @@ namespace XUnitTesten
 {
     public class RecipesControllerTest
     {
-        [Fact]
-        public void IndexInRecipesController()
-        {
-            RecipesController c = new RecipesController();
-            var result = c.Index();
+        private InMemoryDB DB;
+        private WEBProject.Data.WebsiteContext context;
+        private RecipesController rc;
 
-            //result moet een view zijn
-            var viewResult = Assert.IsType<ViewResult>(result);
-            Assert.Null(viewResult.ViewName);
+        public RecipesControllerTest()
+        {
+            DB = new InMemoryDB();
+            context = DB.GetInMemoryDB(true);
+            SeederInMemoryDB.Seed(context);
+            rc = new RecipesController(context);
+        }
+
+        //[Fact]
+        //public void IndexInRecipesController()
+        //{
+        //    RecipesController c = new RecipesController();
+        //    var result = c.Index();
+
+        //    //result moet een view zijn
+        //    var viewResult = Assert.IsType<ViewResult>(result);
+        //    Assert.Null(viewResult.ViewName);
+        //}
+
+        [Fact]
+        public void TestGetRecipes()
+        {
+            var result = rc.GetRecipes(1);
+            Assert.Equal(context.Recipes.ToList()[0], result.ToList()[0]);
+            Assert.Equal(context.Recipes.ToList()[1], result.ToList()[1]);
+            Assert.Equal(context.Recipes.ToList()[2], result.ToList()[2]);
         }
     }
 }
