2195749891a3cfc4cbfeadaccdddad1912a36781
DaicaQuang <35957696+DaicaQuang@users.noreply.github.com>
2019-01-20 12:07:51 +0100

    Add Recipe_Details model

---
+++ WEBProject/Models/Recipe_Details.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WEBProject.Models
+{
+    public class Recipe_Details
+    {
+        [Key]
+        public int DetailID { get; set; }
+        [Required]
+        public string RecipeDetails { get; set; }
+        [ForeignKey("RecipeID")]
+        public int RecipeID { get; set; }
+        public virtual Recipe Recipe { get; set; }
+    }
+}
