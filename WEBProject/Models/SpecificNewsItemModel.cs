e284a443a3320b88819048c015102f2805085ef1
jel07 <jel07@users.noreply.github.com>
2019-01-20 21:36:49 +0100

    Add specific news item

---
+++ WEBProject/Models/SpecificNewsItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBProject.Models
+{
+    public class SpecificNewsItemModel
+    {
+        public List<string> Content { get; set; }
+        public News_Item Item { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
