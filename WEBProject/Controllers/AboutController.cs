e2d02944dfc695da38e6f5a1d83cdca962c22b45
Justin <17042623@student.hhs.nl>
2019-01-21 15:10:15 +0100

    About page fixes added links to about page

--- WEBProject/Controllers/AboutController.cs
+++ WEBProject/Controllers/AboutController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using WEBProject.Data;
+using WEBProject.Models;
 namespace WEBProject.Controllers
 {
     public class AboutController : Controller
     {
+        private readonly WebsiteContext _context;
+
+        public AboutController(WebsiteContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-
+            ViewData["HeaderBackgroundImg"] = _context.Photos.ToList()[58].PhotoPath;
+            ViewBag.News = _context.News_Items.Where(c => c.CategoryID == 3).FirstOrDefault();
             return View();
         }
     }
